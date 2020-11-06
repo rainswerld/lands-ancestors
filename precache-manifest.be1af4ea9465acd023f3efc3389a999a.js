fbc63f24574f8b4666df787d609b3d915f772a91
rainswerld <m.rainrobinson@gmail.com>
2020-11-06 16:39:31 -0700

    Updates

---
+++ precache-manifest.be1af4ea9465acd023f3efc3389a999a.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56638adeef2c5045647ca2a6153871a",
+    "url": "/lands-ancestors/index.html"
+  },
+  {
+    "revision": "487885d40db5c589cdc1",
+    "url": "/lands-ancestors/static/css/2.824a2e44.chunk.css"
+  },
+  {
+    "revision": "892f9b7824d22726ebae",
+    "url": "/lands-ancestors/static/css/main.62cdbe01.chunk.css"
+  },
+  {
+    "revision": "487885d40db5c589cdc1",
+    "url": "/lands-ancestors/static/js/2.e0c6ca81.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/lands-ancestors/static/js/2.e0c6ca81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "892f9b7824d22726ebae",
+    "url": "/lands-ancestors/static/js/main.bd703006.chunk.js"
+  },
+  {
+    "revision": "682047937084ddfb6d37",
+    "url": "/lands-ancestors/static/js/runtime-main.d2e8519f.js"
+  },
+  {
+    "revision": "a4424cb06a3eacc563a5add1a907c72f",
+    "url": "/lands-ancestors/static/media/Ap-2.a4424cb0.jpg"
+  },
+  {
+    "revision": "b996bd1fb485cb60d35b4488a09f5085",
+    "url": "/lands-ancestors/static/media/Ap-picture.b996bd1f.jpg"
+  },
+  {
+    "revision": "0810ac8f9327596eba300e07fb55d8a4",
+    "url": "/lands-ancestors/static/media/BattleScene.0810ac8f.jpg"
+  },
+  {
+    "revision": "215a9a0d9cf3ff26b7509d6b401d088f",
+    "url": "/lands-ancestors/static/media/CondorVillage.215a9a0d.jpg"
+  },
+  {
+    "revision": "f7fed364240e073ef9a5e41a69d8af69",
+    "url": "/lands-ancestors/static/media/DadAndSon.f7fed364.jpg"
+  },
+  {
+    "revision": "3c8269dfa35f204c58ad7b91bf0ccf27",
+    "url": "/lands-ancestors/static/media/KilikandTuhuy.3c8269df.jpg"
+  },
+  {
+    "revision": "1993095cdf434609516e98974811fe5c",
+    "url": "/lands-ancestors/static/media/LOOA-1-FrontCvr.1993095c.jpg"
+  },
+  {
+    "revision": "3e07cf1b97c1e56a100d8b9163437457",
+    "url": "/lands-ancestors/static/media/LOOA-2-FrontCvr.3e07cf1b.jpg"
+  },
+  {
+    "revision": "cd7547a1f527799a639615f9fdd0941b",
+    "url": "/lands-ancestors/static/media/LOOA-3-FrontCvr.cd7547a1.jpg"
+  },
+  {
+    "revision": "ba664a5b4e71460b485a640ff0cd706d",
+    "url": "/lands-ancestors/static/media/LOOA-Bookmark-Side1.ba664a5b.jpg"
+  },
+  {
+    "revision": "55bfc66adf2e5aec01ebb0835b88b463",
+    "url": "/lands-ancestors/static/media/LeastFavoritePart.55bfc66a.jpg"
+  },
+  {
+    "revision": "1dee0429fc91087a3ff42887181aa147",
+    "url": "/lands-ancestors/static/media/Letter1.1dee0429.jpg"
+  },
+  {
+    "revision": "7a614a10734dc265af2195f4a1698f4f",
+    "url": "/lands-ancestors/static/media/Letter2.7a614a10.jpg"
+  },
+  {
+    "revision": "e9fae267a9d1864c3fd1f399f42a0972",
+    "url": "/lands-ancestors/static/media/MalikAndBear.e9fae267.jpg"
+  },
+  {
+    "revision": "56415d97b69c1c73004d8bf1e5eaf9e3",
+    "url": "/lands-ancestors/static/media/OliviaThanksLet.56415d97.jpg"
+  },
+  {
+    "revision": "ad81bdbabd3f7ffe25f987939fdef4ff",
+    "url": "/lands-ancestors/static/media/PhebeThanksLet.ad81bdba.jpg"
+  },
+  {
+    "revision": "d6c7e608a94116ef0d4b3f86f16a046b",
+    "url": "/lands-ancestors/static/media/RioDelSol.d6c7e608.jpg"
+  },
+  {
+    "revision": "04f4eee0387281e97dd5d1caa95ab440",
+    "url": "/lands-ancestors/static/media/RiodelMarSchool.04f4eee0.jpg"
+  },
+  {
+    "revision": "3ea7e81eb88c5883d52ad3c97ab9756e",
+    "url": "/lands-ancestors/static/media/SacredMountain.3ea7e81e.jpg"
+  },
+  {
+    "revision": "7eb259fb68540e634b016f0d04c2f4a8",
+    "url": "/lands-ancestors/static/media/SpanishShips.7eb259fb.jpg"
+  },
+  {
+    "revision": "b0c7f6ec7fa802f4884ade09819a720e",
+    "url": "/lands-ancestors/static/media/SunPicture.b0c7f6ec.jpg"
+  },
+  {
+    "revision": "275c67fb6ce07ce17cfffb2326161829",
+    "url": "/lands-ancestors/static/media/ThankYouLet.275c67fb.jpg"
+  },
+  {
+    "revision": "4b13bb64c5be6f25ca6aee1440212137",
+    "url": "/lands-ancestors/static/media/ThumbsUp.4b13bb64.jpg"
+  },
+  {
+    "revision": "a3f88d15cb31afb38c7a5def62402066",
+    "url": "/lands-ancestors/static/media/VillageScene.a3f88d15.jpg"
+  }
+]);
