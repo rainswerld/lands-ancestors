51f52693256fdae0941895dce03bbda95690333f
rainswerld <m.rainrobinson@gmail.com>
2020-11-24 16:53:40 -0700

    Update student response page to be a grid & update teachers guides covers

--- src/components/Students/StudentRes.js
+++ src/components/Students/StudentRes.js
@@ -1,137 +1,281 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel'
+import { Container, Row, Col } from 'react-bootstrap'
 // import Image from 'react-bootstrap/Image'
 import './StudentRes.css'
 
 function StudentRes() {
   return (
-    <div className="bg-color">
-    <h1 className="student-header">Student Responses</h1>
-    <div className="container">
-      <Carousel>
-        <Carousel.Item interval={1000}>
+    <Container>
+      <Row className="row-time">
+        <Col className="student-pic" md={4} sm={6}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/Ap-2.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={3} sm={6}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/Ap-picture.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={6}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/BattleScene.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={6}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/CondorVillage.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={6}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/Letter1.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={6}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/Letter2.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={6}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/DadAndSon.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={6}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/KilikandTuhuy.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={6}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/LeastFavoritePart.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={6}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/MalikAndBear.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={6}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/OliviaThanksLet.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={6}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/PhebeThanksLet.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={6}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/RioDelSol.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={12}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/ThankYouLet.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={12}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/SacredMountain.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={12}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/RiodelMarSchool.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={12}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/SpanishShips.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={12}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/SunPicture.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={12}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/ThumbsUp.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-        <Carousel.Item interval={1000}>
+        </Col>
+        <Col className="student-pic" md={4} sm={12}>
           <img
             className="d-block w-100"
             src={require('../../assets/student_responses/VillageScene.jpg')}
             alt="student response one" />
-        </Carousel.Item>
-      </Carousel>
-    </div>
-    </div>
+        </Col>
+      </Row>
+    </Container>
+    // <div className="bg-color">
+    // <h1 className="student-header">Student Responses</h1>
+    // <div className="container">
+    //   <Carousel>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/Ap-2.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/Ap-picture.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/BattleScene.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/CondorVillage.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/Letter1.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/Letter2.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/DadAndSon.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/KilikandTuhuy.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/LeastFavoritePart.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/MalikAndBear.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/OliviaThanksLet.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/PhebeThanksLet.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/RioDelSol.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/ThankYouLet.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/SacredMountain.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/RiodelMarSchool.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/SpanishShips.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/SunPicture.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //  DONE
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/ThumbsUp.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //     <Carousel.Item interval={1000}>
+    //       <img
+    //         className="d-block w-100"
+    //         src={require('../../assets/student_responses/VillageScene.jpg')}
+    //         alt="student response one" />
+    //     </Carousel.Item>
+    //   </Carousel>
+    // </div>
+    // </div>
   )
 }
 
--- src/components/TeacherGuides/TeacherGuides.js
+++ src/components/TeacherGuides/TeacherGuides.js
@@ -9,7 +9,7 @@ function BookCard() {
         <Row>
           <Col lg={4} md={12} sm={12}>
             <Card className="book card teach-color">
-                <a href="https://www.amazon.com/Lands-Our-Ancestors-Teachers-Guide/dp/098002725X/ref=sr_1_1?dchild=1&keywords=land+of+our+ancestors+teachers&qid=1603908872&sr=8-11" target="_blank"><Card.Img variant="top" src={require('../../assets/LOOA-1-FrontCvr.jpg')} alt="Lands Of Our Ancestors Book One" /></a>
+                <a href="https://www.amazon.com/Lands-Our-Ancestors-Teachers-Guide/dp/098002725X/ref=sr_1_1?dchild=1&keywords=land+of+our+ancestors+teachers&qid=1603908872&sr=8-11" target="_blank"><Card.Img variant="top" src={require('../../assets/TGFronts_1.jpg')} alt="Lands Of Our Ancestors Book One" /></a>
                 <Card.Body className="card-color">
                   <Card.Title>Book One: Mission Period</Card.Title>
                   <Card.Text>
@@ -23,7 +23,7 @@ function BookCard() {
           </Col>
           <Col lg={4} md={12} sm={12}>
             <Card className="book teach-color">
-                <a href="https://www.amazon.com/Lands-Ancestors-Book-Teachers-Guide/dp/0692162585/ref=sr_1_3?dchild=1&keywords=land+of+our+ancestors+teachers&qid=1603908872&sr=8-3" target="_blank"><Card.Img variant="top" src={require('../../assets/LOOA-2-FrontCvr.jpg')} alt="Lands Of Our Ancestors Book One" /></a>
+                <a href="https://www.amazon.com/Lands-Ancestors-Book-Teachers-Guide/dp/0692162585/ref=sr_1_3?dchild=1&keywords=land+of+our+ancestors+teachers&qid=1603908872&sr=8-3" target="_blank"><Card.Img variant="top" src={require('../../assets/TGFronts_2.jpg')} alt="Lands Of Our Ancestors Book One" /></a>
                 <Card.Body className="card-color">
                   <Card.Title>Book Two: Mexican Rancho Era</Card.Title>
                   <Card.Text>
@@ -37,7 +37,7 @@ function BookCard() {
           </Col>
           <Col lg={4} md={12} sm={12}>
             <Card className="book teach-color">
-                <a href="https://www.amazon.com/Lands-Ancestors-Three-Teachers-Guide/dp/0980027292/ref=sr_1_2?dchild=1&keywords=land+of+our+ancestors+teachers&qid=1603908872&sr=8-2" target="_blank"><Card.Img variant="top" src={require('../../assets/LOOA-3-FrontCvr.jpg')} alt="Lands Of Our Ancestors Book One" /></a>
+                <a href="https://www.amazon.com/Lands-Ancestors-Three-Teachers-Guide/dp/0980027292/ref=sr_1_2?dchild=1&keywords=land+of+our+ancestors+teachers&qid=1603908872&sr=8-2" target="_blank"><Card.Img variant="top" src={require('../../assets/TGFronts_3.jpg')} alt="Lands Of Our Ancestors Book One" /></a>
                 <Card.Body className="card-color">
                   <Card.Title>Book Three: Gold Rush-Statehood</Card.Title>
                   <Card.Text>
