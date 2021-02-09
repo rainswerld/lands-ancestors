6d3fe86e2c5dd39ab2a2179a9497afa35651220e
rainswerld <m.rainrobinson@gmail.com>
2021-02-08 19:33:13 -0700

    Update pdf on true history tab

--- src/components/TrueHistory/TrueHistory.js
+++ src/components/TrueHistory/TrueHistory.js
@@ -9,6 +9,24 @@ function Contact() {
     <TrueHistoryBanner />
       <div className="container">
         <div className="row vimeo-parent">
+        <div className="col-md-3 col-sm-12">
+          <a href="https://vimeo.com/248025380" target="_blank">
+          <img
+            src={require('../../assets/vimeo-graphics/TtTCA-Mission-Opening-black.jpg')}
+            alt="mission opening"
+            className="d-block w-100"
+            /></a>
+            <figcaption className="padding">Click Image to Watch Video</figcaption>
+        </div>
+        <figure className="col-md-3 col-sm-12 figure">
+          <a href="https://vimeo.com/361500261" target="_blank">
+          <img
+            src={require('../../assets/vimeo-graphics/OtherSide-MainImage.jpg')}
+            alt="the other side"
+            className="d-block w-100"
+            /></a>
+            <figcaption className="padding">Click Image to Watch Video</figcaption>
+        </figure>
           <figure className="col-md-3 col-sm-12 figure">
             <a href="https://vimeo.com/286700932" target="_blank">
             <img
@@ -18,24 +36,6 @@ function Contact() {
               /></a>
               <figcaption>Click Image to Watch Video</figcaption>
           </figure>
-          <figure className="col-md-3 col-sm-12 figure">
-            <a href="https://vimeo.com/361500261" target="_blank">
-            <img
-              src={require('../../assets/vimeo-graphics/OtherSide-MainImage.jpg')}
-              alt="the other side"
-              className="d-block w-100"
-              /></a>
-              <figcaption className="padding">Click Image to Watch Video</figcaption>
-          </figure>
-          <div className="col-md-3 col-sm-12">
-            <a href="https://vimeo.com/248025380" target="_blank">
-            <img
-              src={require('../../assets/vimeo-graphics/TtTCA-Mission-Opening-black.jpg')}
-              alt="mission opening"
-              className="d-block w-100"
-              /></a>
-              <figcaption className="padding">Click Image to Watch Video</figcaption>
-          </div>
           <div className="col-md-3 col-sm-12">
           <a href={pdf}>
           <img
