a59c3e31e90557f5776598664c3ba52649fc40b9
rainswerld <m.rainrobinson@gmail.com>
2021-04-01 16:40:27 -0600

    Update padding for teach guides for better responsive design

--- src/components/TeacherGuides/TeacherGuides.js
+++ src/components/TeacherGuides/TeacherGuides.js
@@ -7,13 +7,13 @@ function BookCard() {
     <Container>
       <CardDeck className="teacher-guides justify-content-center">
         <Row>
-          <Col lg={4} md={12} sm={12}>
+          <Col className='book-card' lg={4} md={6} sm={12}>
             <Card className="book card teach-color">
                 <a href="https://www.amazon.com/Lands-Our-Ancestors-Teachers-Guide/dp/098002725X/ref=sr_1_1?dchild=1&keywords=land+of+our+ancestors+teachers&qid=1603908872&sr=8-11" target="_blank"><Card.Img variant="top" src={require('../../assets/TGFronts_1.jpg')} alt="Lands Of Our Ancestors Book One" /></a>
                 <Card.Body className="card-color">
                   <Card.Title>Book One: Mission Period</Card.Title>
                   <Card.Text>
-                    This multi-generational, historical saga begins with Book One by depicting what life might have been like during the Spanish Mission Era, as seen through the eyes of two Chumash boys. 
+                    This multi-generational, historical saga begins with Book One by depicting what life might have been like during the Spanish Mission Era, as seen through the eyes of two Chumash boys.
                   </Card.Text>
                 </Card.Body>
                 <Card.Footer className="buttons">
@@ -21,7 +21,7 @@ function BookCard() {
                 </Card.Footer>
             </Card>
           </Col>
-          <Col lg={4} md={12} sm={12}>
+          <Col className='book-card' lg={4} md={6} sm={12}>
             <Card className="book teach-color">
                 <a href="https://www.amazon.com/Lands-Ancestors-Book-Teachers-Guide/dp/0692162585/ref=sr_1_3?dchild=1&keywords=land+of+our+ancestors+teachers&qid=1603908872&sr=8-3" target="_blank"><Card.Img variant="top" src={require('../../assets/TGFronts_2.jpg')} alt="Lands Of Our Ancestors Book One" /></a>
                 <Card.Body className="card-color">
@@ -35,7 +35,7 @@ function BookCard() {
                 </Card.Footer>
             </Card>
           </Col>
-          <Col lg={4} md={12} sm={12}>
+          <Col className='book-card' lg={4} md={6} sm={12}>
             <Card className="book teach-color">
                 <a href="https://www.amazon.com/Lands-Ancestors-Three-Teachers-Guide/dp/0980027292/ref=sr_1_2?dchild=1&keywords=land+of+our+ancestors+teachers&qid=1603908872&sr=8-2" target="_blank"><Card.Img variant="top" src={require('../../assets/TGFronts_3.jpg')} alt="Lands Of Our Ancestors Book One" /></a>
                 <Card.Body className="card-color">
